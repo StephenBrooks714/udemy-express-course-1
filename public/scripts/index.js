814d720ed8fdca306b9f0e74694e866cc536d04f
Stephen Brooks <codedev1980@gmail.com>
2023-12-20 09:52:48 -0600

    update

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,5 +1,17 @@
-$(document).ready(function(){
-    $("button").click(function(){
-        $("p.list").toggle();
-    });
-});
+// // variables
+// const name = [
+//     "John", "Steve", "Luke", "Robert"
+// ];
+// const job = {
+//     title: "Web Developer", description: "Full stack web developer for hire."
+// }
+// function showItem(){
+//     for(i=0; i < name.length; i++){
+//         console.log(i)
+//     }
+//     console.log(`My job title is ${job.title} & I ${job.description}`)
+// }
+// showItem();
+// function name () => ({
+
+// })
